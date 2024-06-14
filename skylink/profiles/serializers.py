cccf22074b489a7da97ec04fad6cc05e952fd0ce
Saatvik Rao <saatvik.rao@iitgn.ac.in>
2024-06-14 10:51:16 +0530

    added links to profile ids

--- skylink/profiles/serializers.py
+++ skylink/profiles/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import Profile
 
-class ProfileSerializer(serializers.ModelSerializer):
+class ProfileSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Profile
         fields = '__all__'
